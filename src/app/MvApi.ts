9627b1e5f924a9240f81f5e2e9f8cf2da4e5dcce
Yunn Thu Thu <yunn.beebee@outlook.com>
2025-05-01 23:36:55 +0630

    -_-

--- src/app/MvApi.ts
+++ src/app/MvApi.ts
@@ -131,8 +131,9 @@ export const updateMultimedia = async <T>(url: string, data: FormData): Promise<
     console.log('Bearer Token:', token);
   
     try {
-      const response = await axios.post(url, data, {
+      const response = await axios.put(url, data, {
         headers: {
+            'Content-Type': 'multipart/form-data',
           'Authorization': `Bearer ${token}`
         },
       });
