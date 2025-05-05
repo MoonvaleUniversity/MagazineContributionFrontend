4932de7589a43e03f51ea3253625b0ea90d1c272
Yunn Thu Thu <yunn.beebee@outlook.com>
2025-05-05 21:07:27 +0630

    done

--- src/pages/Marketing Coordinator/McStudent.tsx
+++ src/pages/Marketing Coordinator/McStudent.tsx
@@ -158,7 +158,7 @@ export const McStudents  = () => {
 <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700 ">
 <thead className="bg-gray-50 dark:bg-primary-800">
 <tr className="">
-            {["ID", "Name", "Email", "Faculty", "Actions"].map((header, index) => (
+            {["ID", "Name", "Email", "Faculty","Academic Years", "Actions"].map((header, index) => (
               <th key={index} className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">{header}</th>
             ))}
           </tr>
